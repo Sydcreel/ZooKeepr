06187d3def9d07f4a42186e02f12f9cb254095d7
Sydney Creel <sydney.creel@outlook.com>
2021-06-15 13:52:07 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { animals } = require('./data/animals.json');
+
+//takes in req.query as an argument and filters through animals, returning new filtered array.
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        }
+        else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.foreach(trait => {
+            filter = filteredResults.filter(animalanimal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+// get() requires 2 arguments: string that describes the route the client will fetch from
+// and a callback function that executes every time the route is accessed with GET request.
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(PORT, () => {
+    console.log(`api server on port ${PORT}`);
+});
